b8b89e82557061f33dd131ea48829df1811d3e0c
Georgy Borisenko <gosha-bor@yandex.ru>
2016-08-30 13:21:06 +0300

    Options for test_gen

--- test_gen.py
+++ test_gen.py
@@ -1,7 +1,27 @@
 #!/bin/python
 import numpy as np
 import random
-mu = 5.8
+from optparse import OptionParser, OptionError
+
+parser = OptionParser()
+parser.add_option('--mu',
+	action = 'store',        
+	dest = 'mu',       
+	default = '5.8',        
+	help = '') 
+parser.add_option('--sigma_gen',
+	action = 'store',        
+	dest = 'sigma_gen',       
+	default = '1.0',        
+	help = '') 
+parser.add_option('--noise',
+	action = 'store',        
+	dest = 'noise',       
+	default = '0.1',        
+	help = '') 
+(options, args) = parser.parse_args()
+
+mu = float(options.mu)
 U = 16000
 M = 1000
 D = M * U / 5
@@ -10,10 +30,9 @@ dim = 10
 
 k = 10
 
-a = -5.
-b = 5.
+sigma_gen = float(options.sigma_gen)
 
-var = 0.0;
+noise = float(options.noise);
 
 users = np.array([np.array([0.] * dim)] * U)
 items = np.array([np.array([0.] * dim)] * M)
@@ -22,21 +41,21 @@ for user in users:
 	user[0] = mu
 	user[1] = 1.
 	for i in range(2, dim):
-		user[i] = random.uniform(a, b)
+		user[i] = random.gauss(0., sigma_gen)
 
 for item in items:
 	item[0] = 1.
-	item[1] = random.uniform(a, b)
+	item[1] = random.gauss(0., sigma_gen)
 	item[2] = 1.
 	for i in range(3, dim):
-		item[i] = random.uniform(a, b)
+		item[i] = random.gauss(0., sigma_gen)
 
 with open("test.in", "w") as fin:
 	fin.write("{} {} {} {} {}\n".format(k, U, M, D, T))
 	for _ in range(D):
 		u = random.randint(0, U - 1)
 		m = random.randint(0, M - 1)
-		r = np.inner(users[u], items[m]) + random.uniform(-var, var)
+		r = np.inner(users[u], items[m]) + random.gauss(0., noise)
 		fin.write("{} {} {}\n" .format(u, m, r))
 	with open("test.out", "w") as fout:
 		for _ in range(T):
