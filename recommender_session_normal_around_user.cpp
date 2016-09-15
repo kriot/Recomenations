afdcb18a0628cb63b41f53d454967ed5b4ede023
Georgy Borisenko <gosha-bor@yandex.ru>
2016-09-15 20:57:45 +0300

    normal_around_users code skeleton

---
+++ recommender_session_normal_around_user.cpp
@@ -0,0 +1,207 @@
+#include "recommender_session_normal_around_user.hpp"
+#include <opencv2/core/core.hpp>
+#include <opencv2/core/core_c.h>
+#include <iostream>
+#include <fstream>
+
+RecommenderSessionNormalAroundUser::RecommenderSessionNormalAroundUser(
+		Recommender* recommender
+		, DataSession* data
+		, int timeup
+		, double lambda
+		, double regularization_k
+		, long long N)
+	: RecommenderTrainer(recommender)
+	, timeup(timeup) 
+	, N(N)
+	, lambda(lambda) 
+	, regularization_k(regularization_k)
+	, data(data)
+{
+}
+
+RecommenderSessionNormalAroundUser::~RecommenderSessionNormalAroundUser()
+{
+}
+
+void RecommenderSessionNormalAroundUser::Train() {
+//
+// Artcle of method: https://en.wikipedia.org/wiki/Nonlinear_conjugate_gradient_method
+//
+	RandomizeData();
+	EstimateMu();
+	FixAll();
+	double rmse = RMSE();
+	std::cerr << "First RMSE: " << rmse << "\n";
+	std::ofstream lambda_log("lambda.log");
+	std::ofstream rmse_log("rmse.log");
+	std::ofstream speed_log("speed.log");
+	std::ofstream acceleration_log("acceleration.log");
+	auto beginning = std::chrono::system_clock::now();
+
+	const int U = recommender->Users.rows;
+	const int M = recommender->Items.cols;
+	const int dim = recommender->Dimension;
+
+	int steps_per_iteration = 6;
+	bool disable_conjugate = false;
+
+	cv::Mat user_delta_x_prev = cv::Mat::zeros(U, dim, CV_R);
+	cv::Mat movie_delta_x_prev = cv::Mat::zeros(dim, M, CV_R);
+	cv::Mat u0 = user_delta_x_prev.clone();
+	cv::Mat m0 = movie_delta_x_prev.clone();
+	for (int iteration = 0; iteration < N && std::chrono::system_clock::now() - beginning < timeup; ++iteration) {
+		std::cerr << "Iteration: " << iteration << "\n";
+		if (iteration % 10 == 0) {
+			std::cerr << "Iteration: " << iteration << "\n";
+			rmse = RMSE();
+			std::cerr << "RMSE: " << rmse << "\n";
+		}
+
+		if ((iteration / steps_per_iteration) % 2 == 0) { // # train u_id
+
+			// reset m0 after all iterations for mids
+			m0 = cv::Mat::zeros(dim, M, CV_R);
+
+			cv::Mat user_delta_x = cv::Mat::zeros(U, dim, CV_R);
+			// estimate gradient
+			for (const auto& d: data->data) {
+				int u = d.uid;
+				int m = d.iid;
+				int r = d.rating;
+				user_delta_x.row(u) += recommender->Items.col(m).t() * (r - recommender->Predict(u, m)) + regularization_k * recommender->Users.row(u);
+			}
+			// wtf ?
+			for(int u = 0; u < U; ++u) {
+				user_delta_x.at<R>(u, 0) = 0;
+				user_delta_x.at<R>(u, 1) = 0;
+			}
+
+			// estimate number for the method (see article)
+			double beta_pr = iteration < 2 ? 0 : user_delta_x.dot(user_delta_x - user_delta_x_prev) / 
+													user_delta_x_prev.dot(user_delta_x_prev);
+			beta_pr = 0;
+			double beta = std::max(0., beta_pr);
+			
+			if (disable_conjugate)
+				beta = 0;
+
+			// Update the conjugate direction (see article, step 3)
+			u0 = user_delta_x + (u0 * beta);
+
+			// optimize (step 4)
+			double t_num = 0;
+			double t_den = 0;
+			for (const auto& d: data->data) {
+				int u = d.uid;
+				int m = d.iid;
+				int r = d.rating;
+				t_num += cv::Mat((r - recommender->Predict(u, m)) * (u0.row(u) * recommender->Items.col(m))).at<R>(0, 0) + regularization_k * cv::Mat(recommender->Users.row(u) * (u0.row(u).t())).at<R>(0, 0);
+				t_den += std::pow(cv::Mat(u0.row(u) * recommender->Items.col(m)).at<R>(0, 0), 2) + regularization_k * cv::Mat(u0.row(u) * (u0.row(u).t())).at<R>(0, 0); 
+			}
+			double t = t_num / t_den;
+			
+			// apply (step 5)
+			recommender->Users += u0 * t;
+			
+			// save prev
+			user_delta_x_prev = user_delta_x;
+		} else { // # train m_id
+
+			// reset u0 after all iterations for uids
+			u0 = cv::Mat::zeros(U, dim, CV_R);
+			cv::Mat movie_delta_x = cv::Mat::zeros(dim, M, CV_R);
+			// estimate gradient
+			for (const auto& d: data->data) {
+				int u = d.uid;
+				int m = d.iid;
+				int r = d.rating;
+				movie_delta_x.col(m) += recommender->Users.row(u).t() * (r - recommender->Predict(u, m)) + regularization_k * recommender->Items.col(m);
+			}
+
+			// wtf ?
+			for(int m = 0; m < M; ++m) {
+				movie_delta_x.at<R>(0, m) = 0;
+				movie_delta_x.at<R>(2, m) = 0;
+			}
+
+			// estimate number for the method (see article)
+			double beta_pr = iteration < 2 ? 0 : movie_delta_x.dot(movie_delta_x - movie_delta_x_prev) / 
+													movie_delta_x_prev.dot(movie_delta_x_prev);
+			beta_pr = 0;
+			double beta = std::max(0., beta_pr);
+
+			if (disable_conjugate)
+				beta = 0;
+
+			// Update the conjugate direction (see article, step 3)
+			m0 = movie_delta_x + m0 * beta;
+
+			// optimize (step 4)
+			double t_num = 0;
+			double t_den = 0;
+			for (const auto& d: data->data) {
+				int u = d.uid;
+				int m = d.iid;
+				int r = d.rating;
+				t_num += cv::Mat((r - recommender->Predict(u, m)) * (recommender->Users.row(u) * m0.col(m))).at<R>(0, 0) + regularization_k * cv::Mat(recommender->Items.col(m).t() * m0.col(m)).at<R>(0, 0);
+				t_den += std::pow(cv::Mat(recommender->Users.row(u) * m0.col(m)).at<R>(0, 0), 2) + regularization_k * cv::Mat(m0.col(m).t() * m0.col(m)).at<R>(0, 0); 
+			}
+			double t = t_num / t_den;
+
+			// apply (step 5)
+			recommender->Items += m0 * t;
+
+			// save prev
+			movie_delta_x_prev = movie_delta_x;
+		}
+		// FixAll();
+		done_iterations = iteration + 1;
+	}
+//std::cerr << "Time: " << static_cast<int>(std::chrono::system_clock::now() - beginning) / 1000 << " sec.";
+}
+
+double RecommenderSessionNormalAroundUser::RMSE() {
+	double sum = 0;
+	for (const auto& d: data->data) {
+		int u = d.uid;
+		int m = d.iid;
+		int r = d.rating;
+		double error = r - recommender->Predict(u, m);
+		sum += std::pow(error, 2);
+	}
+	return sum / data->data.size();
+}
+
+void RecommenderSessionNormalAroundUser::EstimateMu() {
+	double sum = 0;
+	for (const auto& d: data->data) {
+		int r = d.rating;
+		sum += r; 
+	}
+	mu = sum / data->data.size();
+	//		std::cerr << "Mu: " << mu << "\n";
+}
+// User: (mu,   1, b_u, ... )
+// ItemL ( 1, b_i,   1, ... )
+inline void RecommenderSessionNormalAroundUser::FixUser(UID uid) {
+	recommender->Users.at<R>(uid, 0) = mu;
+	recommender->Users.at<R>(uid, 1) = 1;
+}
+
+inline void RecommenderSessionNormalAroundUser::FixItem(IID iid) {
+	recommender->Items.at<R>(0, iid) = 1.;
+	recommender->Items.at<R>(2, iid) = 1.;
+}
+
+inline void RecommenderSessionNormalAroundUser::FixAll() {
+	for (int i = 0; i < recommender->Users.rows; ++i)
+		FixUser(i);
+	for (int i = 0; i < recommender->Items.cols; ++i)
+		FixItem(i);
+}
+
+void RecommenderSessionNormalAroundUser::RandomizeData() {
+	cv::randn(recommender->Users, 0, 1);
+	cv::randn(recommender->Items, 0, 1);
+}
---
+++ recommender_session_normal_around_user.hpp
@@ -0,0 +1,41 @@
+#pragma once
+#include <chrono>
+#include "recommender.hpp"
+#include "data.hpp"
+#include "recommender_trainer.hpp"
+
+class RecommenderSessionNormalAroundUser: public RecommenderTrainer {
+	public:
+		RecommenderSessionNormalAroundUser(
+				Recommender* recommender
+				, DataSession* data
+				, int timeup
+				, double lambda
+				, double regularization_k
+				, long long N = 1000);
+		~RecommenderSessionNormalAroundUser();
+
+		void Train() override; 
+
+		double RMSE(); 
+
+	private:
+		inline double RMSE_partial(int offset); 
+	public:
+		std::chrono::milliseconds timeup;
+		long long N = 1000;
+		int done_iterations = 0;
+		double lambda;
+		double regularization_k;
+	private:
+		DataSession* data;
+		double mu;
+	private:
+		void EstimateMu(); 
+		// User: (mu,   1, b_u, ... )
+		// ItemL ( 1, b_i,   1, ... )
+		inline void FixUser(UID uid); 
+		inline void FixItem(IID iid); 
+		inline void FixAll(); 
+		void RandomizeData(); 
+};
